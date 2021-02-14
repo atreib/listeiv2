a6bacef60e63df75c263bbdd8a71e4be73c50285
Andre Treib <andre.schumacher.treib@gmail.com>
2021-02-14 20:00:59 -0300

    fix: add setupTests on package.json

--- src/components/pages/dashboard/DashboardPage.spec.tsx
+++ src/components/pages/dashboard/DashboardPage.spec.tsx
@@ -1,7 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { DashboardPage } from './DashboardPage';
+
 describe('Dashboard page testes', () => {
-  it('Should', () => {
-    expect(true).toBe(true);
+  it('Should render correctly', () => {
+    const component = shallow(<DashboardPage />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('Should show page name on h1', () => {
+    const component = shallow(<DashboardPage />);
+    const z = component.find('h1').length;
+    expect(z).toBe(1);
   });
 });
-
-export {};
