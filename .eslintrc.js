7e17387aebb202e6c4f99b6d0cb307a09cf4f10d
André Treib <andre.treib@eldorado.org.br>
2021-02-14 12:49:12 -0300

    chore: config prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,9 @@ module.exports = {
   },
   extends: [
     "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
   ],
   rules: {
     "@typescript-eslint/explicit-module-boundary-types": "off"
