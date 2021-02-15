1328a1cbda15102ac761b239add2973162d121cf
Andre Treib <andre.schumacher.treib@gmail.com>
2021-02-15 14:36:55 -0300

    chore: halfway

--- src/components/pages/dashboard/DashboardPage.spec.tsx
+++ src/components/pages/dashboard/DashboardPage.spec.tsx
@@ -1,7 +1,10 @@
-import { DashboardPage as sut } from './DashboardPage';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { DashboardPage as Sut } from './DashboardPage';
 
 describe('Dashboard page testes', () => {
   it('Should render correctly', () => {
-    expect(sut).toBe(sut);
+    const { getByText } = render(<Sut />);
+    expect(getByText(/Dashboard/i)).toBeInTheDocument();
   });
 });
