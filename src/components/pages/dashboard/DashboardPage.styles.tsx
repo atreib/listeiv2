745dabdf96271cb7e31623df11539e22c8f8e60b
Andre Treib <andre.schumacher.treib@gmail.com>
2021-02-16 00:52:03 -0300

    feat: created todo list layout structure

--- src/components/pages/dashboard/DashboardPage.styles.tsx
+++ src/components/pages/dashboard/DashboardPage.styles.tsx
@@ -1,8 +1,31 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const ShoppingList = styled.div`
   margin: 0;
   padding: 0;
   wisth: 100%;
   display: flex;
+  flex-direction: column;
+  align-items: stretch;
+`;
+
+export const ListTitle = styled.div`
+  padding: 12px;
+  font-size: 1.5rem;
+`;
+
+export const List = styled.ul`
+  margin: 0;
+  padding: 4px 12px;
+  list-styled-type: none;
+`;
+
+export const Product = styled.li`
+  padding: 8px 0px;
+  font-size: 1.5rem;
+`;
+
+export const NewProduct = styled.div`
+  padding: 12px;
+  font-size: 1.5rem;
 `;
--- src/components/pages/dashboard/DashboardPage.tsx
+++ src/components/pages/dashboard/DashboardPage.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import { Container } from './DashboardPage.styles';
+import { ShoppingList, ListTitle, NewProduct, List, Product } from './DashboardPage.styles';
 
 export const DashboardPage = () => {
-  return <Container>Dashboard</Container>;
+  const __mock_product__ = {
+    label: 'Iogurte',
+    quantity: 2,
+    unityPrice: 12.5,
+    totalPrice: 25.0,
+  };
+
+  const __mock_list__ = [__mock_product__, __mock_product__];
+
+  return (
+    <ShoppingList>
+      <ListTitle>Lista de compras</ListTitle>
+      <div>
+        <List>{__mock_list__ && __mock_list__.map((product, i) => <Product key={i}>{product.label}</Product>)}</List>
+      </div>
+      <NewProduct>New item</NewProduct>
+    </ShoppingList>
+  );
 };
 
 export default DashboardPage;
