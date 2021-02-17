38b251a5d587346bdafdbbbecc9d33890394dbbb
Andre Treib <andre.schumacher.treib@gmail.com>
2021-02-17 18:48:37 -0300

    feat: create context && use created context on dashboard

---
+++ src/contexts/shoppingList/ShoppingList.tsx
@@ -0,0 +1,90 @@
+import React, { useState, createContext, ReactChild, ReactChildren } from 'react';
+import { generate } from '../../helpers/uuid';
+import { ProductModel } from '../../models';
+
+interface ComponentProps {
+  children: ReactChild | ReactChildren;
+}
+
+type ProviderType = {
+  products: ProductModel[];
+  increaseProductQuantity: (productId: string) => void;
+  decreaseProductQuantity: (productId: string) => void;
+  removeProduct: (productId: string) => void;
+  addProduct: (productName: string) => void;
+};
+
+const initialProviderValues: ProviderType = {
+  products: [],
+  increaseProductQuantity: () => null,
+  decreaseProductQuantity: () => null,
+  removeProduct: () => null,
+  addProduct: () => null,
+};
+
+const ShoppingListContext = createContext<ProviderType>(initialProviderValues);
+
+const ShoppingListProvider = ({ children }: ComponentProps) => {
+  const [products, setProducts] = useState<ProductModel[]>([]);
+
+  /**
+   * Adds a new product to the shopping list state
+   * * @param productName: (string) inserted product label
+   */
+  const addProduct = (productName: string) => {
+    const newitem = {
+      id: generate(),
+      label: productName,
+      quantity: 1,
+      unityPrice: 0.0,
+      totalPrice: 0.0,
+    };
+
+    if (products) setProducts([...products, newitem]);
+    if (!products) setProducts([newitem]);
+  };
+
+  /**
+   * Removes a product from shopping list
+   * @param id: (string) id of the removed product
+   */
+  const removeProduct = (id: string) => {
+    setProducts(products.filter((x) => x.id !== id));
+  };
+
+  /**
+   * Increase product quantity by one
+   * @param id: (string) id of the product being modified
+   */
+  const increaseProductQuantity = (id: string) => {
+    setProducts(
+      products.map((x) => {
+        if (x.id === id) return { ...x, quantity: x.quantity + 1 };
+        return x;
+      }),
+    );
+  };
+
+  /**
+   * Decrease product quantity by one
+   * @param id: (string) id of the product being modified
+   */
+  const decreaseProductQuantity = (id: string) => {
+    setProducts(
+      products.map((x) => {
+        if (x.id === id) return { ...x, quantity: x.quantity === 1 ? 1 : x.quantity - 1 };
+        return x;
+      }),
+    );
+  };
+
+  return (
+    <ShoppingListContext.Provider
+      value={{ products, increaseProductQuantity, decreaseProductQuantity, removeProduct, addProduct }}
+    >
+      {children}
+    </ShoppingListContext.Provider>
+  );
+};
+
+export { ShoppingListContext, ShoppingListProvider };
