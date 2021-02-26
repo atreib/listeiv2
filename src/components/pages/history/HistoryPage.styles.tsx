a11f3db7c797550ae99c7b949509232518719572
Andre Treib <andre.schumacher.treib@gmail.com>
2021-02-25 22:57:45 -0300

    chore: implements layout structure

---
+++ src/components/pages/history/HistoryPage.styles.tsx
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+import { colors } from '../../../helpers/theme';
+
+export {
+  AppInput,
+  AppButton,
+  AppList,
+  AppConfirmDialog,
+  AppPromptDialog,
+  AppListItem,
+  AppIconButton,
+  AppCheckbox,
+} from './../../utils';
+export { NewListIcon, DeleteIcon, PlusIcon, MinusIcon, LogoIcon, AddProductIcon } from './../../utils/icons';
+
+export const ShoppingListHistory = styled.div`
+  margin: 0;
+  padding: 0;
+  wisth: 100%;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const PageTitle = styled.div`
+  padding: 12px;
+  font-size: 1.5rem;
+  color: ${colors.contrastPrimary};
+  background-color: ${colors.primary};
+  display: flex;
+  flex-orientation: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ListWrapper = styled.div`
+  padding: 0px 0px 4px 0px;
+`;
+
+export const ProductLabelWrapper = styled.div`
+  flex: 1;
+  max-width: 85%;
+  display: flex;
+  align-items: center;
+  font-size: 1.2rem;
+`;
+
+export const ProductNameLabel = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-right: 0px;
+`;
--- src/components/pages/history/HistoryPage.tsx
+++ src/components/pages/history/HistoryPage.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import {
+  ShoppingListHistory,
+  LogoIcon,
+  ListWrapper,
+  AppList,
+  AppIconButton,
+  TitleWrapper,
+  PageTitle,
+} from './HistoryPage.styles';
+import { colors } from './../../../helpers/theme';
+import { ShoppingListModel } from './../../../models';
 
 export const HistoryPage = () => {
-  return <h1>History</h1>;
+  const products: ShoppingListModel[] = [];
+
+  return (
+    <ShoppingListHistory>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Listei</title>
+        <style>{`body { background-color: ${colors.background}; color: ${colors.contrastBackground}; }`}</style>
+      </Helmet>
+      <PageTitle>
+        <TitleWrapper>
+          <AppIconButton bgColor={colors.primary}>
+            <LogoIcon />
+          </AppIconButton>
+          Histórico
+        </TitleWrapper>
+      </PageTitle>
+      <ListWrapper>
+        {products && (
+          <AppList>
+            {products.map((product) => (
+              <div key={product.id}>{product.date}</div>
+            ))}
+          </AppList>
+        )}
+        {(!products || products.length === 0) && 'Não há nada no histórico'}
+      </ListWrapper>
+    </ShoppingListHistory>
+  );
 };
 
 export default HistoryPage;
