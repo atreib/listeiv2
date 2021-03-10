9f99e2eb8412e5d5dd9a9c794dcf781786c198d9
Andre Treib <andre.schumacher.treib@gmail.com>
2021-03-10 19:55:59 -0300

    feat: create confirm box for opening old list

--- src/components/pages/history/ShoppingListItem.tsx
+++ src/components/pages/history/ShoppingListItem.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { colors } from '../../../helpers/theme';
 import { ShoppingListModel } from '../../../models';
-import { AppListItem } from '../../utils';
+import { AppConfirmDialog, AppListItem } from '../../utils';
 import { LeftIcon } from './HistoryPage.styles';
 
 interface ComponentProps {
@@ -9,25 +9,42 @@ interface ComponentProps {
 }
 
 export const ShoppingListItem = ({ shoppingList }: ComponentProps) => {
+  const [isLoadingOldShoppingList, setIsLoadingOldShoppingList] = useState(false);
+
+  const onOpenOldList = () => {
+    return;
+  };
+
   return (
-    <AppListItem
-      icon={<LeftIcon fontSize="small" />}
-      key={shoppingList.id}
-      paddingTop="16px"
-      paddingBottom="16px"
-      paddingLeft="0px"
-      fontSize="1.3rem"
-      leftActionBackgrond={colors.primary}
-      leftActionFontColor={colors.contrastPrimary}
-      leftActionText={'Carregar'}
-      enableSwipeLeft={true}
-      onSwipedLeft={() => {
-        return;
-      }}
-    >
-      <>
-        {shoppingList.date.toLocaleDateString()} ({shoppingList.products.length} produtos)
-      </>
-    </AppListItem>
+    <>
+      {isLoadingOldShoppingList && (
+        <AppConfirmDialog
+          title="Você tem certeza?"
+          description="Você deseja abrir esta lista antiga? Sua lista atual será apagada..."
+          dialogOpen={isLoadingOldShoppingList}
+          setDialogOpen={setIsLoadingOldShoppingList}
+          successBtnText="ABrir lista"
+          testId="openOldListConfirm"
+          fnSuccess={() => onOpenOldList()}
+        />
+      )}
+      <AppListItem
+        icon={<LeftIcon fontSize="small" />}
+        key={shoppingList.id}
+        paddingTop="16px"
+        paddingBottom="16px"
+        paddingLeft="0px"
+        fontSize="1.3rem"
+        leftActionBackgrond={colors.primary}
+        leftActionFontColor={colors.contrastPrimary}
+        leftActionText={'Carregar'}
+        enableSwipeLeft={true}
+        onSwipedLeft={() => setIsLoadingOldShoppingList(true)}
+      >
+        <>
+          {shoppingList.date.toLocaleDateString()} ({shoppingList.products.length} produtos)
+        </>
+      </AppListItem>
+    </>
   );
 };
