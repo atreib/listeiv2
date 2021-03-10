397ebeea1243355963b96581508c542f1037eca0
Andre Treib <andre.schumacher.treib@gmail.com>
2021-03-10 20:38:00 -0300

    fix: app tests after changes

--- src/components/layout/AppLayout.spec.tsx
+++ src/components/layout/AppLayout.spec.tsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import { AppLayout } from './AppLayout';
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('App layout main structure', () => {
   it('Should correctly render our tabs structure', () => {
-    const { getByTestId } = render(<AppLayout>teste</AppLayout>);
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <AppLayout>teste</AppLayout>
+      </BrowserRouter>,
+    );
     const tabs = getByTestId('appLayoutNavTabs');
     expect(tabs).toBeTruthy();
   });
 
   it('Should correctly render our content structure', () => {
-    const { getByTestId } = render(<AppLayout>teste</AppLayout>);
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <AppLayout>teste</AppLayout>
+      </BrowserRouter>,
+    );
     const content = getByTestId('appLayoutContent');
     expect(content).toBeTruthy();
   });
 
   it('Should render content structure with correct content value', () => {
-    const { getByTestId } = render(<AppLayout>teste</AppLayout>);
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <AppLayout>teste</AppLayout>
+      </BrowserRouter>,
+    );
     const content = getByTestId('appLayoutContent');
     expect(content).toBeTruthy();
     expect(content.innerHTML).toContain('teste');
